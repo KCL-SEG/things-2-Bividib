c44c046660b3dbd6a227c66e14e0d7e531b2cba1
Bividib <masterbivas@gmail.com>
2023-10-20 00:05:27 +0100

    add thing form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,18 @@
 """Forms of the project."""
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your forms here.
+class ThingForm(forms.Form):
+
+    def clean(self): 
+        cleaned_description = int(self.cleaned_data['quantity'])
+
+        if not cleaned_description in range(0,51): 
+            self.add_error('quantity','Quantity must be between 0 and 50 inclusive.')
+       
+
+    name = forms.CharField(max_length=35)
+    description = forms.CharField(max_length=120,widget=forms.Textarea())
+    quantity = forms.NumberInput() 
+
+
