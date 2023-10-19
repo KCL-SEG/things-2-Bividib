2f56cf063f18f53052aafa6695d18faac4dbb8ed
Bividib <masterbivas@gmail.com>
2023-10-20 00:09:39 +0100

    update quantity field in thing form

--- things/forms.py
+++ things/forms.py
@@ -4,15 +4,15 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class ThingForm(forms.Form):
 
-    def clean(self): 
-        cleaned_description = int(self.cleaned_data['quantity'])
+    # def clean(self): 
+    #     cleaned_description = int(self.cleaned_data['quantity'])
 
-        if not cleaned_description in range(0,51): 
-            self.add_error('quantity','Quantity must be between 0 and 50 inclusive.')
+    #     if not cleaned_description in range(0,51): 
+    #         self.add_error('quantity','Quantity must be between 0 and 50 inclusive.')
        
 
     name = forms.CharField(max_length=35)
     description = forms.CharField(max_length=120,widget=forms.Textarea())
-    quantity = forms.NumberInput() 
+    quantity = forms.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(50)],widget=forms.NumberInput())
 
 
